3b6d24114313369554c6ac004423098149e0d0a6
Imri <imri294@gmail.com>
2018-06-01 12:38:21 +0300

    trying

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+class App extends Component {
+  state = {
+    inputValue: "",
+    todos: []
+  };
+
+  onChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  getRandomId = () => {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 16; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  };
+
+  addTodo = e => {
+    e.preventDefault();
+    const todoToAdd = {
+      name: this.state.inputValue,
+      id: this.getRandomId()
+    };
+    const currentTodos = [...this.state.todos, todoToAdd];
+    this.setState({ todos: currentTodos, inputValue: "" });
+    console.log(currentTodos);
+  };
+
+  removeTodo = todo => {
+    const filtered = this.state.todos.filter(el => el !== todo);
+    this.setState({ todos: filtered });
+  };
+
+  clearTodos = e => {
+    e.preventDefault();
+    this.setState({ todos: [] });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Todo App</h2>
+        <form>
+          <input
+            value={this.state.inputValue}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Add a mission"
+          />
+          <button onClick={this.addTodo} type="submit">
+            Add Task
+          </button>
+          <button onClick={this.clearTodos}>Clear Todos</button>
+        </form>
+        <div>
+          <ul>
+            {this.state.todos.map((todo, index) => {
+              return (
+                <li onClick={() => this.removeTodo(todo)} key={index}>
+                  {todo.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
